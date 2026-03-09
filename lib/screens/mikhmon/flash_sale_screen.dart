90f790d038fb24ba782d05fa914166eab4b414fd
eaglelax <joachimaxelzongo@gmail.com>
2026-03-09 10:26:29 +0000

    Fix mikhmon flash sale: remove unused MikhmonService, fix deprecated value param

--- lib/screens/mikhmon/flash_sale_screen.dart
+++ lib/screens/mikhmon/flash_sale_screen.dart
@@ -4,7 +4,6 @@ import 'package:qr_flutter/qr_flutter.dart';
 import '../../config/theme.dart';
 import '../../models/site.dart';
 import '../../services/api_client.dart';
-import '../../services/mikhmon_service.dart';
 import '../../utils/formatters.dart';
 
 class FlashSaleScreen extends StatefulWidget {
@@ -16,7 +15,6 @@ class FlashSaleScreen extends StatefulWidget {
 }
 
 class _FlashSaleScreenState extends State<FlashSaleScreen> {
-  final _service = MikhmonService();
   List<Map<String, dynamic>> _profiles = [];
   List<Map<String, dynamic>> _points = [];
   int? _selectedPointId;
@@ -139,7 +137,7 @@ class _FlashSaleScreenState extends State<FlashSaleScreen> {
           Padding(
             padding: const EdgeInsets.fromLTRB(16, 12, 16, 4),
             child: DropdownButtonFormField<int>(
-              value: _selectedPointId,
+              initialValue: _selectedPointId,
               decoration: const InputDecoration(
                 labelText: 'Point de vente',
                 prefixIcon: Icon(Icons.storefront),
